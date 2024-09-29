12c56add530fafceebee84182f5f7ea2f595555d
MrRandy231 <100807773+MrRandy231@users.noreply.github.com>
2024-09-29 15:28:00 -0400

    Update app.test.js

--- app.test.js
+++ app.test.js
@@ -1,2 +1,8 @@
-
 touch app.test.js
+
+const { execSync } = require('child_process');
+
+test('Verificar salida de Hola Mundo', () => {
+    const output = execSync('node app.js').toString();
+    expect(output.trim()).toBe('Hola Mundo');
+});
